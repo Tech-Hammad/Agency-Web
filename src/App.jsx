83c15974516f5a59d03ac1e87afd6565ff637581
Hammad Ahmad <hammadahmad.9022@gmail.com>
2026-02-11 00:47:52 +0500

    Installed Tailwind and Created Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", theme === "dark");
+  }, [theme]);
+
   return (
-    <div>
-
+    <div className="relative">
+      <Navbar theme={theme} setTheme={setTheme} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
