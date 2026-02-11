fa5f3e924df6f5db7a7b815764f3c9d98dc3e300
Hammad Ahmad <hammadahmad.9022@gmail.com>
2026-02-11 17:59:07 +0500

    Created Hero Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
 
 const App = () => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
 
   useEffect(() => {
     document.documentElement.classList.toggle("dark", theme === "dark");
@@ -11,6 +12,7 @@ const App = () => {
   return (
     <div className="dark:bg-black relative">
       <Navbar theme={theme} setTheme={setTheme} />
+      <Hero />
     </div>
   );
 };
