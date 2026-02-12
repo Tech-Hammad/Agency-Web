4fb54ae9c3f0190c3b12ac90f67d467d0fc48bce
Hammad Ahmad <hammadahmad.9022@gmail.com>
2026-02-12 15:51:11 +0500

    Added Teams Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./Components/Hero";
 import TrustedBy from "./Components/TrustedBy";
 import Services from "./Components/Services";
 import OurWork from "./Components/OurWork";
+import Teams from "./Components/Teams";
 
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
@@ -19,6 +20,7 @@ const App = () => {
       <TrustedBy />
       <Services />
       <OurWork />
+      <Teams />
     </div>
   );
 };
